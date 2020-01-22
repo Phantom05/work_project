0b2d87168669bc4044f285ae0846fe689f369172
phantom05 <monster2jy@gmail.com>
2020-01-22 23:34:03 +0900

    확장성 번외편! 번외 주석 아래는 따로 안보셔도 되요!

--- project/week3/확장성/index.js
+++ project/week3/확장성/index.js
@@ -1,12 +1,25 @@
 document.getElementById('btn').addEventListener('click',function(){
-  const img_title = document.createElement('h2');
-  const img_box = document.createElement('div');
-  const img = document.createElement('img');
-  img_title.innerHTML = 'Hello, Image'
-  img_box.classList.add('img_box');
-  img.src = 'https://source.unsplash.com/random';
-  img.classList.add('test_img');
-  img_box.append(img_title,img);
+const input = document.getElementById('input');
+const inputValue = input ? input.value :'';
+  for(let i = 0; i < 10; i++){
 
-  document.getElementById('target').append(img_box);
+    const img_title      = document.createElement('h2');
+    const img_dim        = document.createElement('div');
+    const img_hover_text = document.createElement('p');
+    const img_box        = document.createElement('div');
+    const img            = document.createElement('img');
+
+    img_hover_text.innerHTML=`Programming! ${inputValue}`
+    img_title.innerHTML = 'Hello, Image'
+    img.src = 'https://source.unsplash.com/random';
+    img_hover_text.classList.add('img_hover_text');
+    img_dim.classList.add('img_dim');
+    img_box.classList.add('img_box');
+    img.classList.add('img');
+
+    img_dim.append(img_hover_text);
+    img_box.append(img_dim,img_title,img);
+    document.getElementById('target').append(img_box);
+  }
+  document.documentElement.scrollTop = document.body.scrollHeight;
 })

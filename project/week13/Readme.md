d6658e25e31b3fc89dc34931c9888cd798ae669c
phantom05 <monster2jy@gmail.com>
2020-04-12 13:51:16 +0900

    업데이트 13주차 업데이트 !

--- project/week13/Readme.md
+++ project/week13/Readme.md
@@ -81,22 +81,188 @@ function add1(x,y){
 
 + `closure ` 설명
 
+```js
+
+// NOTE: 클로저
+function count() {
+  let number = 0;
+  return function add() {
+    number++;
+    return number;
+  }
+}
+
+let upCount = count();
+
+console.log(
+  upCount()
+);
+console.log(
+  upCount()
+);
+console.log(
+  upCount()
+);
+console.log(
+  upCount()
+);
+```
+
+
+
 + `function prototype` 과 `class 인스턴스 ` 설명
 
   - new 인스턴스의 사용법 설명
-
 + 프로토타입 참고자료
 
   + [참고자료1]([https://medium.com/@bluesh55/javascript-prototype-%EC%9D%B4%ED%95%B4%ED%95%98%EA%B8%B0-f8e67c286b67](https://medium.com/@bluesh55/javascript-prototype-이해하기-f8e67c286b67))
   + [참고자료2](http://www.nextree.co.kr/p7323/)
   + [참고자료3](http://insanehong.kr/post/javascript-prototype/)
 
+
++ 고차함수 설명
+
+```js
+
+// NOTE: 고차함수 basic
+function attitude(original, replacemenet) {
+  return function (source) {
+    return source.replace(original, replacemenet);
+  }
+}
+var snakify = attitude(/millenials/ig, "Snake People");
+
+console.log(
+  snakify("The Millenials are always up to something.")
+);
+```
+
+
+
++ 재귀함수 설명
+
+```js
+// NOTE: 재귀 
+function increase(count) {
+  console.log(count);
+  count++;
+  if (count > 10) {
+    return;
+  }
+  increase(count);
+}
+increase(0);
+```
+
+
+
++ 비동기 설명
+
+![1_FA9NGxNB6-v1oI2qGEtlRQ](https://user-images.githubusercontent.com/33567964/79060785-5766a380-7cc4-11ea-9e32-60e725d41792.png)
+
+***ajax가 Web Api에 들어가 있었네요 비동기 처리들도 따로 메모리를 뜨는게 아니라 web API으로 들어가게되네요***
+
++ 컬렉션과 생성기 설명
+
+  + `Map` 설명
+
+  ```js
+  var map1 = new Map();
+  var obj1 = { a: 15 };
+  var obj2 = { b: 10 };
+  
+  map1.set(obj1, "User");
+  map1.set(obj2, "User1");
+  
+  console.log(map1);
+  for(let keyname of map1.keys()){
+    console.log(keyname);
+  }
+  
+  for(let valueName of map1.values()){
+    console.log(valueName);
+  }
+  
+  console.clear()
+  for(let data of map1.entries() ){
+    // console.log(data);
+    console.log(data[0]);
+    console.log(data[1]);
+  }
+  ```
+
   
 
+  + `Set` 설명
 
+  ```js
+  var set1 = new Set();
+  
+  set1.add('hello');
+  set1.add('world');
+  
+  console.log(
+    set1.keys()
+  );
+  console.log(
+    set1.values()
+  );
+  ```
+
+  
+
++ `Promise` 설명
+
+```js
+// NOTE: Promise
+function promise2(intiData){
+  return new Promise(function(resolve,reject){
+    setTimeout(()=>{
+      resolve(intiData);
+    },1000);
+  })
+};
+
+function promise3(intiData){
+  return new Promise(function(resolve,reject){
+    setTimeout(()=>{
+      resolve(intiData + 10);
+    },1000);
+  })
+};
+
+promise2(50)
+.then(promise3)
+.then(function(data){
+  console.log(data+10);
+  document.getElementById('test').innerHTML = data;
+});
+```
+
+
+
++ `async/await` 설명
+
+```js
+// NOTE: async/ await
+async function test(){
+  var data = await dataComunication();
+  document.getElementById('test').innerHTML = data;
+
+  var data = await dataComunication();
+  document.getElementById('test').innerHTML = data;
+}
+
+```
+
++ 자료구조, 빅오 표기법 기본 설명
+
++ 알고리즘 추천 사이트
+  + [코드워](https://www.codewars.com/)
+  + [프로그래머스](https://programmers.co.kr/)
+  + [백준](https://www.acmicpc.net/)
 
 
 
 <hr />
-
 - 이번주 잔여 8h 30m
---
+++ project/week13/day2/index.js
@@ -0,0 +1,218 @@
+console.log(`Hello Programing`);
+
+
+// NOTE: 클로저
+function count() {
+  let number = 0;
+  return function add() {
+    number++;
+    return number;
+  }
+}
+
+
+let upCount = count();
+
+
+console.log(
+  upCount()
+);
+console.log(
+  upCount()
+);
+console.log(
+  upCount()
+);
+console.log(
+  upCount()
+);
+
+
+// HOF
+
+// NOTE: 고차함수 basic
+function attitude(original, replacemenet) {
+  return function (source) {
+    return source.replace(original, replacemenet);
+  }
+}
+
+var snakify = attitude(/millenials/ig, "Snake People");
+
+console.log(
+  snakify("The Millenials are always up to something.")
+);
+
+console.clear();
+
+
+// NOTE: 재귀 
+function increase(count) {
+  console.log(count);
+  count++;
+  if (count > 10) {
+    return;
+  }
+  increase(count);
+}
+increase(0);
+
+// 순수함수에만, 재귀를 쓰는 경우가 많습니다.
+
+console.clear();
+// NOTE: ES7
+// 오바라이팅 + 제네릭
+
+var map1 = new Map();
+var obj1 = { a: 15 };
+var obj2 = { b: 10 };
+
+map1.set(obj1, "User");
+map1.set(obj2, "User1");
+
+console.log(map1);
+for(let keyname of map1.keys()){
+  console.log(keyname);
+}
+
+for(let valueName of map1.values()){
+  console.log(valueName);
+}
+
+console.clear()
+for(let data of map1.entries() ){
+  // console.log(data);
+  console.log(data[0]);
+  console.log(data[1]);
+}
+
+
+console.log(
+  map1
+);
+console.log(
+  ['abc','bbb']
+);
+
+
+console.clear();
+
+var set1 = new Set();
+
+set1.add('hello');
+set1.add('world');
+
+console.log(set1);
+
+
+console.log(
+  set1.keys()
+);
+console.log(
+  set1.values()
+);
+
+
+for(let data of set1.values()){
+  console.log(data);
+}
+
+
+// console.log(
+//   map1.get(obj1)
+// );
+
+// console.log(
+//   map1.keys()
+// );
+// console.log(
+//   obj1
+// );
+
+// console.log(
+//   map1.values()
+// );
+// console.log(
+//   map1.entries()
+// );
+
+// const map1 = {
+//   '{ a: 15 }':"User"
+// }
+
+
+// 깊은복사 얕은복사,
+
+
+// for(let i = 0 ; i < 10; i ++){
+//   obj[i]
+// }
+
+
+console.clear();
+
+var promise1 = new Promise(function(resolve,reject){
+  setTimeout(()=>{
+    resolve('hello');
+  },3000);
+});
+
+
+promise1.then(function(data){
+  console.log(data,'promise result');
+});
+
+promise1.then(function(data){
+  console.log(data,'promise result1');
+})
+
+
+// NOTE: Promise
+function promise2(intiData){
+  return new Promise(function(resolve,reject){
+    setTimeout(()=>{
+      resolve(intiData);
+    },1000);
+  })
+};
+
+function promise3(intiData){
+  return new Promise(function(resolve,reject){
+    setTimeout(()=>{
+      resolve(intiData + 10);
+    },1000);
+  })
+};
+
+promise2(50)
+.then(promise3)
+.then(function(data){
+  console.log(data+10);
+  document.getElementById('test').innerHTML = data;
+});
+
+// all,every
+
+
+// NOTE: async/ await
+async function test(){
+  var data = await dataComunication();
+  document.getElementById('test').innerHTML = data;
+
+  var data = await dataComunication();
+  document.getElementById('test').innerHTML = data;
+}
+
+
+// 제너레이터
+// function* gen(){
+//   yield 
+//   while(true){
+//     gen()
+//   }
+// }
+
+// gen.next()
+// gen.next()
+// gen.next()
+// gen.next()

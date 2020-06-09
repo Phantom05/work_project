22608c0e2e7503a28aec09de9ffda9367437e77b
phantom05 <monster2jy@gmail.com>
2020-06-10 01:44:02 +0900

    업데이트 : 임시 걸ㄹ프 셋팅

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const sourcemaps = require("gulp-sourcemaps");
+const autoprefixer = require("gulp-autoprefixer");
+// const { paths, assets, setting } = require("./gulpfile.config");
+
+const _path = {
+  src: {
+    style: "./project/**/*.scss",
+  },
+  dist: {
+    style: "./project/build/",
+  },
+};
+const setting = {
+  styles: {
+    scss_option: {
+      outputStyle: "compressed",
+      indentType: "tab",
+      indentWidth: 1,
+      precision: 3,
+      sourceComments: false,
+      errLogToConsole: true,
+      // importer: moduleImporter({ basedir: path.join(__dirname, 'public/src/assets/scss/') })
+      // includePaths: [paths.styles.src]
+    },
+    rootFontSize: 16,
+  },
+  scripts: {
+    gulp_babel_minify: {
+      mangle: {
+        keepClassName: false,
+        booleans: false,
+        deadcode: true,
+        simplify: true,
+        removeConsole: true,
+        numericLiterals: true,
+      },
+    },
+  },
+};
+
+function build_styles() {
+  return (
+    gulp
+      .src(_path.src.style)
+      .pipe(sourcemaps.init())
+      // .pipe(sassGlob())
+      .pipe(sass(setting.styles.scss_option).on("error", sass.logError))
+      // .pipe(sourcemaps.write())
+      .pipe(autoprefixer("last 2 versions"))
+      .pipe(gulp.dest(_path.dist.style))
+  );
+}
+
+gulp.task(`styles`, build_styles);
+/**
+ * Watch
+ */
+gulp.task(`watch`, () => {
+  gulp.watch(_path.src.style, [`styles`]);
+});
+
+/**
+ * =================================================================+
+ * @ gulp tasks process
+ * =================================================================+
+ */

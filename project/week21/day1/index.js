2b9216e65c4998553579984ce21866d7ecf46020
phantom05 <monster2jy@gmail.com>
2020-06-26 01:42:21 +0900

    업데이트 : 커피 프로그램 간단히 세팅

--- project/week21/day1/index.js
+++ project/week21/day1/index.js
@@ -1,5 +1,33 @@
+const inputSearch = document.getElementById("inputSearch");
+const inputSearchBtn = document.getElementById("inputSearchBtn");
+const searchLiset = document.getElementById("searchLiset");
+
+const config = {
+  keyword: "",
+  coffeeMenuList: [],
+};
 async function main() {
   const { data } = await axios.get(api.coffeeData);
-  console.log(data.list, "d");
+  config.coffeeMenuList = data.list;
+  console.log(data.list);
 }
 main();
+
+inputSearch.addEventListener("keyup", function (e) {
+  if (e.key === "Enter") {
+    inputSearchBtn.click();
+  }
+});
+
+inputSearchBtn.addEventListener("click", function (e) {
+  config.keyword = inputSearch.value.trim();
+  inputSearch.value = "";
+  console.log(config, "config");
+  const containList = config.coffeeMenuList.filter((item) => {
+    return item.name.indexOf(config.keyword) !== -1;
+  });
+  console.log(containList, "containList");
+
+  searchLiset.innerHTML = "";
+  $(searchLiset).html(containList.map((item) => item.name).join(","));
+});
